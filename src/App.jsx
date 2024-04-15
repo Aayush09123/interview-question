d2dbba43d000cd5a6a555dfaa41d53fe1742f768
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-15 15:10:14 +0530

    interview question solution for component re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState({
+    value: 0
+  })
+  // const [multipliedValue, setMultipliedValue] = useState(1)
+
+  // let multipliedValue = value * 5
+
+  // const multiplybyfive = () => {
+  //   setMultipliedValue(value * 5)
+  //   setValue(value + 1);
+  // }
+
+  const clickMe = () => {
+    setValue({
+      value: 0
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Main Value: {value.value}</h1>
+      <button onClick={clickMe}>
+        Click to multiply by 5
+      </button>
+      {/* <h2>Multiplied Value: {multipliedValue}</h2> */}
     </>
   )
 }
